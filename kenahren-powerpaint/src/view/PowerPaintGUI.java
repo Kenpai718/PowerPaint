93236510d9d747887ddacdaefee8857ec5954634
Ken Ahrens <kenahren@gmail.com>
2020-12-01 13:54:36 -0800

    made toolbar
    -still in drafting phase. Most of gui is setup now.
    -added toolbar to frame
    -added buttons to toolbar
    -added a new package for "actions" that will represent the buttons for the tools in the toolbar

--- kenahren-powerpaint/src/view/PowerPaintGUI.java
+++ kenahren-powerpaint/src/view/PowerPaintGUI.java
@@ -1,17 +1,32 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.Action;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+
+import actions.PencilAction;
+import actions.LineAction;
+import actions.RectangleAction;
+import actions.EllipseAction;
+import actions.EraserAction;
+
 
 public class PowerPaintGUI implements PropertyChangeListener {
 
@@ -39,35 +54,71 @@ public class PowerPaintGUI implements PropertyChangeListener {
 	/** The top menu for this GUI. */
 	private JMenuBar myMenuBar;
 
-	/** A button group for radio buttons. */
-	//private final ButtonGroup myTopGroup;
-
 	/** Top tools menu bar. */
 	private JMenu myToolsMenu;
 
 	/** Top file menu bar. */
 	private JMenu myFileMenu;
 
-	/** Top file menu bar. */
+	/** Top help menu bar. */
 	private JMenu myHelpMenu;
 
 	/** The image icon to show in the window title and about window. */
 	private ImageIcon myImageIcon = new ImageIcon("./images/w.gif");
+	
+    /** A list of color actions. */
+    private ButtonGroup myToolButtons;
+	
+    /** A list of tool actions from actions package. */
+    private List<Action> myToolActions;
+    
+    private JToolBar myToolBar;
 
 	public PowerPaintGUI() {
+		
+		
 		setupGUI();
 		setupTopMenu();
+		
 
 	}
 
 	public void setupGUI() {
+		//initialize jframe
 		myFrame = new JFrame("PowerPaint");
 		myFrame.setVisible(true);
 		myFrame.setSize(500, 500);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//initialize fields like menus 
+		myPanel = new PaintPanel();
+		myToolBar = new JToolBar();
+		myToolActions = new ArrayList<Action>();
+		myToolButtons = new ButtonGroup();
 
+		//add buttons and menu options
+		setupAction();
 		setupTopMenu();
+		
+		
+		myFrame.add(myToolBar, BorderLayout.SOUTH);
 
+	}
+	
+	public void setupAction() {
+		
+        myToolActions.add(new PencilAction(myPanel));
+        myToolActions.add(new LineAction(myPanel));
+        myToolActions.add(new RectangleAction(myPanel));
+        myToolActions.add(new EllipseAction(myPanel));
+        myToolActions.add(new EraserAction(myPanel));
+        
+        // associate tool buttons with actions
+        for (final Action act : myToolActions) {
+        	final JToggleButton tb = new JToggleButton(act);
+            myToolButtons.add(tb);
+            myToolBar.add(tb);
+        }
 	}
 
 	public void setupTopMenu() {
@@ -85,19 +136,19 @@ public class PowerPaintGUI implements PropertyChangeListener {
 		myToolsMenu = new JMenu("Tools");
 		myMenuBar.add(myToolsMenu);
 
-		myHelpMenu = new JMenu("About");
+		myHelpMenu = new JMenu("Help");
 		myMenuBar.add(myHelpMenu);
 		
 		
 
 	}
 
-
 	
 	class ExitAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			//dispose(); idk how this works but tom has it
 			System.exit(0);
 			
 		}
